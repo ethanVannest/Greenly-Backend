bdbd4d02e3d07b7716c23903fb64aec4b921a836
ethanvannest <ethan3vannest@gmail.com>
2022-11-05 08:00:23 -0400

    Cart schema created

---
+++ models/Cart.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+    Name: {
+        type:String,
+        required: true
+    },
+    Price: {
+        type: Integer,
+        required: true
+    }, 
+    Description: {
+        type: String,
+        required: true
+    },
+    Stocked: {
+        type:Boolean,
+        required: true
+    }, 
+    Image: {
+        type: String,
+        required: true
+    }
+})
---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Products: require('./Products')
+    
+}

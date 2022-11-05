7abe2349dbd58b00a0ce25e365e2d4e08afd3f88
ethanvannest <ethan3vannest@gmail.com>
2022-11-05 08:14:12 -0400

    create controller created

--- controllers/Products.ctrls.js
+++ controllers/Products.ctrls.js
@@ -2,11 +2,22 @@ const db = require('../models')
 
 const index = (req, res) => {
     db.Products.find({}, (error, items) => {
-        if(error) return res.status(400).json({
+        if(error) 
+        return res.status(400).json({
             error: error.message
         })
         return res.status(200).json({
             items
         })
+    })
+}
+
+const create = (req, res) => {
+    db.Products.create(req.body, (error, createdItems) => {
+        if (error) 
+        return res.status(400).json({
+            error: error.message
+        })
+        return res.status(200).json(createdItems)
     })
 }

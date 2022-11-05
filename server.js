455bd703f3fffb3b74989de367fcba6273c9b5ae
ethanvannest <ethan3vannest@gmail.com>
2022-11-05 07:30:36 -0400

    server running

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const routes = require('')
 
 const PORT = process.env.PORT || 3000
 
-require('.config/db.connection')
+require('./config/db.connection')
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}` )

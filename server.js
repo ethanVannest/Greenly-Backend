bb3e7ce09eb0f81a1d9b9eef08ed3a106cdd270e
ethanvannest <ethan3vannest@gmail.com>
2022-11-05 07:26:45 -0400

    Server access point

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const express = require('express')
+const app = express()
+const routes = require('')
+
 const PORT = process.env.PORT || 3000
 
-require
+require('.config/db.connection')
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}` )
+})

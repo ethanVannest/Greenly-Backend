c0ca7b2b8d6cdd9d7fa58d91ebc9e9a9b542b07d
ethanvannest <ethan3vannest@gmail.com>
2022-11-16 14:29:12 -0700

    removed secondary route

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { index, show } = require('../controllers/Products.ctrls')
 
-router.get('/products', index)
+router.get('/', index)
 
 router.get('/:id', show)
 

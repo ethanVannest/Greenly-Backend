392f7fc2a9aeb17efd3a7b0e6dfff02206ac01d4
ethanvannest <ethan3vannest@gmail.com>
2022-11-22 18:29:03 -0700

    debugged home route

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000
 
 connectDB();
 
-app.use('/api/products', routes )
+app.use('/', routes )
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}` )

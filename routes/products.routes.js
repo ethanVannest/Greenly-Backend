a0d001c4cdd5dac71c5292ae017c2b53c6554536
ethanvannest <ethan3vannest@gmail.com>
2022-11-23 14:16:46 -0700

    recent push

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,10 +2,17 @@ const express = require('express')
 const router = express.Router()
 const { index, show, update } = require('../controllers/Products.ctrls')
 
+
 const cors = require('cors')
 
+// app.use(function(req, res, next) {
+// res.header("Access-Control-Allow-Origin", "http://localhost:3000/cart"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
 router.get('/', index)
-router.put('/:id', cors(), update)
+router.put('/:id', update)
 router.get('/:id', show)
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,16 +2,34 @@ const express = require('express')
 const app = express()
 const connectDB = require('./config/db.connection')
 const routes = require('./routes/products.routes')
-
+const cors = require('cors')
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }))
 
+app.use(
+    cors({
+        origin: "*"
+    })
+)
+  
+//   app.put('/:id', (req, res) => {
+//     request(
+//       { url: 'http://localhost:3001/' },
+//       (error, response, body) => {
+//         if (error || response.statusCode !== 200) {
+//           return res.status(500).json({ type: 'error', message: err.message });
+//         }
+  
+//         res.json(JSON.parse(body));
+//       }
+//     )
+//   });
 const PORT = process.env.PORT || 3000
 
 connectDB();
 
-app.use('/api/products', routes )
+app.use('/api/products/', routes )
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}` )
